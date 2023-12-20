c2e95cb386bd2bf1e51b247b9e0352b955791407
AudiHazael <hazaelaudi@gmail.com>
2023-12-20 21:04:54 +0100

    first commit

---
+++ JavaScript/imageSwiper.js
@@ -0,0 +1,14 @@
+// Make sure this code is placed within a document ready or window load event
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('#product-slider', {
+    // Use the unique ID
+    direction: 'vertical', // Set the direction to vertical
+    loop: true, // Enable loop mode for continuous scrolling
+
+    // Pagination settings
+    pagination: {
+      el: '.swiper-pagination', // Selector for pagination container
+      clickable: true, // Make pagination bullets clickable
+    },
+  });
+});
